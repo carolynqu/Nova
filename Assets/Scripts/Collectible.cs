8a4aebc6e1f68f0f57db999448754165a0c77859
SydlWang <sydney.l.wang@duke.edu>
2023-03-05 15:54:14 +0100

    level one pretty much done

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -28,28 +28,4 @@ public class Collectible : MonoBehaviour
         }
     }
 
-    //IEnumerator CollectAnimation()
-    //{
-    //    Vector3 startPosition = transform.position;
-    //    Vector3 endPosition = transform.position + Vector3.up * 1.5f;
-    //    float startRotation = 0;
-    //    float endRotation = 1500;
-    //    Color startColor = Color.white;
-    //    Color endColor = Color.white;
-    //    endColor.a = 0;
-    //    Vector3 startScale = new Vector3(1, 1, 1);
-    //    Vector3 endScale = new Vector3(1.5f, 1.5f, 1.5f);
-    //    float startTime = Time.time;
-    //    float endTime = Time.time + animationDuration;
-    //    while (Time.time < endTime)
-    //    {
-    //        float t = 1 - ((endTime - Time.time) / animationDuration);
-    //        mySpriteRenderer.color = Color.Lerp(startColor, endColor, t);
-    //        transform.localScale = Vector3.Lerp(startScale, endScale, t);
-    //        transform.rotation = Quaternion.AngleAxis(Mathf.Lerp(startRotation, endRotation, t), Vector3.up);
-    //        transform.position = Vector3.Lerp(startPosition, endPosition, t);
-    //        yield return null;
-    //    }
-    //    Destroy(gameObject);
-    //}
 }
